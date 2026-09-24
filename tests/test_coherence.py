cc33dc481e4156bea363137210ebddd145ec521b
Jessie James Jarder <smolfiddle@gmail.com>
2026-09-24 15:48:43 +0800

    chore: bump version 3.5.2 -> 3.6.0 for WSL setup

--- tests/test_coherence.py
+++ tests/test_coherence.py
@@ -33,7 +33,7 @@ class TestBenchmark:
         import pahebatcher
         import pahebatcher.models
         import pahebatcher.utils
-        assert pahebatcher.__version__ == "3.5.2"
+        assert pahebatcher.__version__ == "3.6.0"
 
 
 class TestDocsCoherence:
